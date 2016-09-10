e8c16eca64979c4e15494eaa36c811f71e0f0450
Giacomo Bagnoli <gbagnoli@gmail.com>
2016-09-10 23:41:49 +0100

    Install keepass and dropbox

--- site-cookbooks/ubik/recipes/ppa.rb
+++ site-cookbooks/ubik/recipes/ppa.rb
@@ -48,3 +48,11 @@ end
 apt_repository 'fasd' do
   uri 'ppa:aacebedo/fasd'
 end
+
+apt_repository 'dropbox' do
+  uri 'http://linux.dropbox.com/ubuntu/'
+  distribution node['lsb']['codename']
+  components ['main']
+  keyserver 'pgp.mit.edu'
+  key '5044912E'
+end
