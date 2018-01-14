a1e426e56e8a41bda1020700fe50cdd8f47f1b11
Giacomo Bagnoli <gbagnoli@gmail.com>
2018-01-14 12:05:34 +0000

    Upgrade vagrant version

--- site-cookbooks/ubik/recipes/packages.rb
+++ site-cookbooks/ubik/recipes/packages.rb
@@ -21,7 +21,7 @@ packages = {
   'skype' => 'https://repo.skype.com/latest/skypeforlinux-64.deb',
   'keybase' => 'https://prerelease.keybase.io/keybase_amd64.deb',
   'steam' => 'https://steamcdn-a.akamaihd.net/client/installer/steam.deb',
-  'vagrant' => 'https://releases.hashicorp.com/vagrant/2.0.0/vagrant_2.0.0_x86_64.deb',
+  'vagrant' => 'https://releases.hashicorp.com/vagrant/2.0.1/vagrant_2.0.1_x86_64.deb',
   'dropbox' => 'https://linux.dropbox.com/packages/ubuntu/dropbox_2015.10.28_amd64.deb',
   'slack' => 'https://downloads.slack-edge.com/linux_releases/slack-desktop-2.8.1-amd64.deb'
 }
