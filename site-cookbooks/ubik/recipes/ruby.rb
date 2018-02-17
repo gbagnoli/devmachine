f64799774e9feecf17d54e1228f03abcf99b7115
Giacomo Bagnoli <gbagnoli@gmail.com>
2018-02-17 16:01:29 +0000

    Fix directory creation for chefdk rbenv

--- site-cookbooks/ubik/recipes/ruby.rb
+++ site-cookbooks/ubik/recipes/ruby.rb
@@ -4,7 +4,10 @@ include_recipe 'ruby_rbenv::user'
 
 user = 'giacomo'
 chefdk_dir = "/home/#{user}/.rbenv/versions/chefdk"
-directory chefdk_dir
+directory chefdk_dir do
+  recursive :true
+  owner user
+end
 
 rbenv_script 'finish-rbenv-chefdk-plugin-install' do
   rbenv_version 'chefdk'
