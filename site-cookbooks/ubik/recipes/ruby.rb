c0d36dd74a0f63775c74eb023c6568db8e26aefd
Giacomo Bagnoli <gbagnoli@gmail.com>
2018-02-17 16:05:23 +0000

    Fix: value was meant to be bool not symbol

--- site-cookbooks/ubik/recipes/ruby.rb
+++ site-cookbooks/ubik/recipes/ruby.rb
@@ -5,7 +5,7 @@ include_recipe 'ruby_rbenv::user'
 user = 'giacomo'
 chefdk_dir = "/home/#{user}/.rbenv/versions/chefdk"
 directory chefdk_dir do
-  recursive :true
+  recursive true
   owner user
 end
 
