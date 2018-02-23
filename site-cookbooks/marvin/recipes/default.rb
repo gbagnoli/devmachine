193933d2aedb7a6ad890500ea14b119fb123978c
Giacomo Bagnoli <gbagnoli@gmail.com>
2018-02-23 16:51:26 +0000

    Nginx should be installed creating vhosts
    [skip ci]

--- site-cookbooks/marvin/recipes/default.rb
+++ site-cookbooks/marvin/recipes/default.rb
@@ -1,5 +1,5 @@
-include_recipe 'marvin::oauth2_proxy'
-include_recipe 'marvin::thelounge'
-
 node.override['nginx']['default_site_enabled'] = false
 include_recipe 'nginx::default'
+
+include_recipe 'marvin::oauth2_proxy'
+include_recipe 'marvin::thelounge'
