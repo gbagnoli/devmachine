208200f03f3ecfbb93cd9b81fc5676523e5caafe
Giacomo Bagnoli <gbagnoli@gmail.com>
2018-02-24 15:23:43 +0000

    Dnscrypt proxy: bump to version 2, install from binary

--- site-cookbooks/dnscrypt_proxy/attributes/default.rb
+++ site-cookbooks/dnscrypt_proxy/attributes/default.rb
@@ -1,6 +1,10 @@
 # frozen_string_literal: true
 
-default['dnscrypt_proxy']['src_dir'] = '/usr/local/src/dnscrypt-autoinstall'
-default['dnscrypt_proxy']['revision'] = 'master'
-default['dnscrypt_proxy']['repository'] = 'https://github.com/simonclausen/dnscrypt-autoinstall.git'
-default['dnscrypt_proxy']['resolver'] = 'fvz-rec-gb-brs-01'
+# old directory of the autoinstall script, now used for cleanup
+default['dnscrypt_proxy']['autoinstall_src_dir'] = '/usr/local/src/dnscrypt-autoinstall'
+
+default['dnscrypt_proxy']['version'] = '2.0.4'
+default['dnscrypt_proxy']['repository_url'] = 'https://github.com/jedisct1/dnscrypt-proxy/'
+default['dnscrypt_proxy']['arch'] = 'linux_x86_64'
+
+default['dnscrypt_proxy']['listen_address'] = '0.0.0.0'
--- site-cookbooks/dnscrypt_proxy/recipes/default.rb
+++ site-cookbooks/dnscrypt_proxy/recipes/default.rb
@@ -1,7 +1,91 @@
 # frozen_string_literal: true
 
-if node['platform'] == 'ubuntu'
-  include_recipe 'dnscrypt_proxy::ubuntu'
-else
-  include_recipe 'dnscrypt_proxy::autoinstall'
+include_recipe 'dnscrypt_proxy::cleanup'
+
+srcfile = '/usr/src/dnscrypt_proxy.tar.gz'
+tmpdir = '/tmp/dnscrypt-proxy'
+arch = node['dnscrypt_proxy']['arch']
+
+remote_file srcfile do
+  repo = node['dnscrypt_proxy']['repository_url']
+  v = node['dnscrypt_proxy']['version']
+  source "#{repo}releases/download/#{v}/dnscrypt-proxy-#{arch}-#{v}.tar.gz"
+  notifies :run, 'execute[unpack-dnscrypt_proxy]', :immediately
+end
+
+execute 'unpack-dnscrypt_proxy' do
+  action :nothing
+  command "mkdir -p #{tmpdir} && tar xzf #{srcfile} -C #{tmpdir}"
+  notifies :run, 'execute[install-dnscrypt-proxy]', :immediately
+end
+
+user 'dnscrypt' do
+  system true
+  shell '/bin/false'
+  group 'nogroup'
+end
+
+execute 'install-dnscrypt-proxy' do
+  action :nothing
+  cwd "#{tmpdir}/#{arch.sub('_', '-')}"
+  command 'cp dnscrypt-proxy /usr/bin/ && setcap "cap_net_bind_service=+ep" /usr/bin/dnscrypt-proxy'
+end
+
+directory tmpdir do
+  action :delete
+  recursive true
+end
+
+config = '/etc/dnscrypt-proxy.toml'
+
+template config do
+  source 'dnscrypt-proxy.toml.erb'
+  owner 'dnscrypt'
+  group 'nogroup'
+  mode '0444'
+end
+
+systemd_unit 'dnscrypt-proxy.service' do
+  verify false
+  content <<~EOU
+    [Unit]
+    Description=DNSCrypt client proxy
+    Documentation=man:dnscrypt-proxy(8)
+    Requires=dnscrypt-proxy.socket
+    After=network.target
+    Before=nss-lookup.target
+    Wants=nss-lookup.target
+
+    [Install]
+    Also=dnscrypt-proxy.socket
+    WantedBy=multi-user.target
+
+    [Service]
+    Type=simple
+    NonBlocking=true
+    User=dnscrypt
+    Group=nogroup
+    ExecStart=/usr/bin/dnscrypt-proxy -config #{config}
+  EOU
+  action %i[create]
+end
+
+systemd_unit 'dnscrypt-proxy.socket' do
+  verify false
+  content <<~EOU
+    [Unit]
+    Description=dnscrypt-proxy listening socket
+
+    [Socket]
+    ListenStream=#{node['dnscrypt_proxy']['listen_address']}:53
+    ListenDatagram=#{node['dnscrypt_proxy']['listen_address']}:53
+    NoDelay=true
+    DeferAcceptSec=1
+
+    [Install]
+    WantedBy=sockets.target
+  EOU
+  action %i[create enable]
+  notifies :start, 'systemd_unit[dnscrypt-proxy.service]', :immediately
+  notifies :enable, 'systemd_unit[dnscrypt-proxy.service]', :immediately
 end
