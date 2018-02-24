6600b9650a8a756fbef7ae7b9b74d74f79e39bef
Giacomo Bagnoli <gbagnoli@gmail.com>
2018-02-24 01:22:33 +0000

    Fix pre-push commit cd
    [skip ci]

--- hooks/pre-push.sh
+++ hooks/pre-push.sh
@@ -2,7 +2,7 @@
 
 set -eu
 
-pushd "$(dirname "$GIT_DIR")" >/dev/null
+pushd "$(git rev-parse --show-toplevel)" >/dev/null
 bundle check >/dev/null || bundle install
 bundle exec rubocop site-cookbooks roles
 bundle exec foodcritic -B site-cookbooks -R roles
