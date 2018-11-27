a3f296c49ca816c6743bcd3717f82dfafa31629f
Giacomo Bagnoli <gbagnoli@gmail.com>
2018-11-27 00:33:43 +0000

    bender: temporarly enable root login

--- roles/bender.rb
+++ roles/bender.rb
@@ -1,5 +1,14 @@
 name 'bender'
 description 'configure bender'
+default_attributes(
+  'ssh-hardening' => {
+    'ssh' => {
+      'server' => {
+        'allow_root_with_key' => true
+      }
+    }
+  }
+)
 run_list [
   'role[server]',
   'recipe[bender::default]'
