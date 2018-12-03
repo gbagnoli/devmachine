c1c89deb46840582bb7a593d75ebd5df6d0696ba
Giacomo Bagnoli <gbagnoli@gmail.com>
2018-12-03 21:17:29 +0000

    Fix attributes

--- site-cookbooks/bender/attributes/containers.rb
+++ site-cookbooks/bender/attributes/containers.rb
@@ -4,6 +4,6 @@ default['bender']['containers']['marvin']['id'] = 2
 default['bender']['containers']['marvin']['image'] = 'ubuntu:18.04'
 # ssh auto-forwarded from base_ssh + id => 22
 # format is [[host_port, port], [host_port2, port2]] or nil
-default['bender']['containers']['marvin']['forwarded_ports'] = nil
+default['bender']['containers']['marvin']['forwarded_ports'] = [[32400, 32400]]
 # if set, this will nat 1:1 the ipv6 address to the container
 default['bender']['containers']['marvin']['external_ipv6'] = nil
