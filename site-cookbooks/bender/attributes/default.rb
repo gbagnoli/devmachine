5df10dfe6fd2c9c04ec8b2e94f7d8ebbb9329e85
Giacomo Bagnoli <gbagnoli@gmail.com>
2018-12-03 00:58:28 +0000

    bender: Resolve containers from host

--- site-cookbooks/bender/attributes/default.rb
+++ site-cookbooks/bender/attributes/default.rb
@@ -5,9 +5,11 @@ default['bender']['network']['host']['ipv6']['addr'] = '2a01:4f8:191:22eb::2/64'
 default['bender']['network']['containers']['interface'] = 'lxdbr0'
 
 default['bender']['network']['containers']['base_ssh'] = 3000
-default['bender']['network']['containers']['ipv4']['addr'] = '172.24.24.1/24'
+default['bender']['network']['containers']['ipv4']['addr'] = '172.24.24.1'
+default['bender']['network']['containers']['ipv4']['addr_cidr'] = '172.24.24.1/24'
 default['bender']['network']['containers']['ipv4']['network'] = '172.24.24.0/24'
-default['bender']['network']['containers']['ipv6']['addr'] = 'fd05:f439:6192:1b03::1/64'
+default['bender']['network']['containers']['ipv6']['addr'] = 'fd05:f439:6192:1b03::1'
+default['bender']['network']['containers']['ipv6']['addr_cidr'] = 'fd05:f439:6192:1b03::1/64'
 default['bender']['network']['containers']['ipv6']['network'] = 'fd05:f439:6192:1b03::0/64'
 default['bender']['storage']['containers']['name'] = 'data'
 default['bender']['storage']['containers']['source'] = '/data/containers'
--- site-cookbooks/bender/recipes/containers.rb
+++ site-cookbooks/bender/recipes/containers.rb
@@ -5,11 +5,14 @@ config = <<~HEREDOC
   - name: #{node['bender']['network']['containers']['interface']}
     type: bridge
     config:
-      ipv4.address: #{node['bender']['network']['containers']['ipv4']['addr']}
-      ipv6.address: #{node['bender']['network']['containers']['ipv6']['addr']}
+      ipv4.address: #{node['bender']['network']['containers']['ipv4']['addr_cidr']}
+      ipv6.address: #{node['bender']['network']['containers']['ipv6']['addr_cidr']}
       ipv4.firewall: false
       ipv6.firewall: false
       ipv6.dhcp.stateful: true
+      raw.dnsmasq: |
+        auth-zone=lxd
+        dns-loop-detect
 
   storage_pools:
   - name: #{node['bender']['storage']['containers']['name']}
@@ -28,6 +31,36 @@ HEREDOC
 
 lxd_config 'bender' do
   content config
+end
+
+%w[lxd-host-dns-start lxd-host-dns-stop].each do |script|
+  path = "/usr/local/bin/#{script}"
+  template path do
+    source "#{script}.erb"
+    variables(
+      interface: node['bender']['network']['containers']['interface'],
+      address: node['bender']['network']['containers']['ipv4']['addr'],
+      domain: 'lxd'
+    )
+    mode 0o750
+  end
+end
+
+systemd_unit 'lxd-host-dns.service' do
+  content <<~EOU
+    [Unit]
+    Description=LXD host DNS service
+    After=multi-user.target
+     [Service]
+    Type=simple
+    ExecStart=/usr/local/bin/lxd-host-dns-start
+    RemainAfterExit=true
+    ExecStop=/usr/local/bin/lxd-host-dns-stop
+    StandardOutput=journal
+     [Install]
+    WantedBy=multi-user.target
+  EOU
+  action %i[create enable start]
 end
 
 node['bender']['containers'].each do |name, conf|
