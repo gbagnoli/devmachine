23665d78abe4705fb4df9d920eaf8634c9703a28
Giacomo Bagnoli <gbagnoli@gmail.com>
2018-12-08 00:22:36 +0000

    Configure oauth2proxy on marvin

--- site-cookbooks/marvin/attributes/default.rb
+++ site-cookbooks/marvin/attributes/default.rb
@@ -2,3 +2,13 @@ default['marvin']['thelounge']['userid'] = '2000'
 default['marvin']['thelounge']['groupid'] = '2000'
 default['marvin']['thelounge']['home'] = '/var/lib/thelounge'
 default['marvin']['thelounge']['port'] = 6677
+
+default['marvin']['oauth2_proxy']['http_port'] = '4180'
+default['marvin']['oauth2_proxy']['upstream_port'] = 4189
+default['marvin']['oauth2_proxy']['auth_provider'] = 'google'
+default['marvin']['oauth2_proxy']['redirect-url'] = 'https://www.tigc.eu/oauth2/callback'
+# need to set this in the secrets file
+default['marvin']['oauth2_proxy']['authenticated_emails'] = nil
+default['marvin']['oauth2_proxy']['client-secret'] = nil
+default['marvin']['oauth2_proxy']['client-id'] = nil
+default['marvin']['oauth2_proxy']['cookie-secret'] = nil
--- site-cookbooks/marvin/recipes/oauth2_proxy.rb
+++ site-cookbooks/marvin/recipes/oauth2_proxy.rb
@@ -1,7 +1,52 @@
 include_recipe 'server::oauth2_proxy'
 
-oauth2_proxy_site 'tigc' do
-  auth_provider 'google'
-  http_address '127.0.0.1:4180'
-  https_address '127.0.0.1:4181'
+if node['marvin']['oauth2_proxy']['client-secret'].nil?
+  Chef::Log.error('Skipping oauth2_proxy configuration as no attrs for secrets were found')
+  return
 end
+
+conf = node['marvin']['oauth2_proxy']
+
+user 'oauth2proxy' do
+  system true
+  shell '/bin/false'
+  group 'nogroup'
+end
+
+file '/etc/oauth2_proxy/tigc.emails.txt' do
+  content conf['authenticated_emails'].sort.join("\n")
+  mode 0o400
+  owner 'oauth2proxy'
+  group 'nogroup'
+  notifies :restart, 'service[oauth2_proxy-tigc]', :immediately
+end
+
+oauth2_proxy_site 'tigc' do
+  auth_provider conf['auth_provider']
+  http_address  "127.0.0.1:#{conf['http_port']}"
+  upstreams ["http://127.0.0.1:#{conf['upstream_port']}/"]
+  redirect_url conf['redirect-url']
+  authenticated_emails_file '/etc/oauth2_proxy/tigc.emails.txt'
+  cookie_secret conf['cookie-secret']
+  client_id conf['client-id']
+  client_secret conf['client-secret']
+end
+
+directory '/etc/systemd/system/oauth2_proxy-tigc.service.d'
+
+file '/etc/systemd/system/oauth2_proxy-tigc.service.d/override.conf' do
+  content <<~HEREDOC
+    [Service]
+    User=oauth2proxy
+    Group=nogroup
+  HEREDOC
+  notifies :run, 'execute[oauth2proxy-reload-tigc]', :immediately
+end
+
+execute 'oauth2proxy-reload-tigc' do
+  action :nothing
+  notifies :restart, 'service[oauth2_proxy-tigc]'
+  command 'systemctl daemon-reload'
+end
+
+service 'oauth2_proxy-tigc'
