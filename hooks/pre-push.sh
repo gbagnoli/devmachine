ca95c03db21a0385f6191dda575153bddf110a01
Giacomo Bagnoli <gbagnoli@gmail.com>
2018-12-17 16:51:57 +0000

    Fix pre-push commit to only check fabfile with isort

--- hooks/pre-push.sh
+++ hooks/pre-push.sh
@@ -7,5 +7,5 @@ bundle check >/dev/null || bundle install
 bundle exec rubocop site-cookbooks roles
 bundle exec foodcritic -B site-cookbooks -R roles
 pipenv run -- black --check fabfile.py
-pipenv run -- isort
+pipenv run -- isort fabfile.py
 popd >/dev/null
