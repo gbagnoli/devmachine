96a03d12a6f7d9e6e5be03964805a8b8fe2ea6a8
Giacomo Bagnoli <gbagnoli@gmail.com>
2018-12-19 13:48:00 +0000

    Add datadog handler to pentolino and bloodsea

--- roles/container.rb
+++ roles/container.rb
@@ -1,6 +1,10 @@
 name 'container'
 description 'common configurations for containers'
 
+run_list [
+  'recipe[datadog::dd-handler]'
+]
+
 override_attributes(
   'os-hardening' => {
     'components' => {
