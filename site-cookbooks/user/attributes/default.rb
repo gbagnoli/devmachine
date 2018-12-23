547db6a2fdee88fd50b9de08bb28cecf9f0682fe
Giacomo Bagnoli <gbagnoli@gmail.com>
2018-12-23 12:36:56 +0100

    Added oldpc key to the list of ssh keys

--- site-cookbooks/user/attributes/default.rb
+++ site-cookbooks/user/attributes/default.rb
@@ -22,4 +22,8 @@ default['user']['ssh_authorized_keys'] = [{
   name: 'giacomo@android',
   keytype: 'ssh-rsa',
   pubkey: 'AAAAB3NzaC1yc2EAAAABIwAAAgEAtVrcXBTJG5U/CfpgO3C6DXsgQSJZpRAaNvfa5kq+/z/Jog7boatVUPpWmq0IUzO6cdHhQ04bNBlb5ycPWxACQC0tXRmdpUBW3CXyLMn595s1W9zJGV7hdZDX9t2EWox4ZshKsxu31PZ6RArBLTjki8EGwh66TdG+PPcrTwtZA32rjdOm12K560ySH8keSKSX1I298ITe/9AOzWqhl4cN/MywiDuIq8/ZozRSbffc5hOzW8JpRLHtccnAF15kvuyffHnUpBkES0sKHCBdXIp83uha/ahNPbTri6pmFJ/Vsc+UZkhINYK+63jWxNafCgWrMi8acK5qiU+Q+C3ujyHL8Lk4kjVrM6nqSDfc33OXOGkH5Z7QQFm1jOxbUuuM7oKNiIWd2uGiTRcvQVZto5rlaSPAfiM2O13bHKRMOj//wYsDeF1XcZPU42X+g/HIqbEy8r2sBGPxlX0n/z89lgBfXDxNIxm4Nhj7LEhV6OpfU6RCuK2K79tbQ7Ajh0QHzkiRQuLlet3KeeGJzs9LBC2fHpR+0d8ZATIwXB4RwRyprpUmln8sjmLzrn18lH/n+/zZ+GLiLPC1EYtJRR8DaT4gW/7fJxVZtW3hzhnkzwvPOL9YVgS17RfxweZE4ftXZAzrnPQZ0OZ/fxnuRey+BDp5BFIiMBAOFo2uzvHVnsD/Jj8=' # rubocop:disable Metrics/LineLength
+}, {
+  name: 'jdoe@oldpc',
+  keytype: 'ssh-ed25519',
+  pubkey: 'AAAAC3NzaC1lZDI1NTE5AAAAIJ7IqCcv/ybJQ8Y0u60y3JltCDqgQ+UZyTzjVwiYY1m+'
 }]
