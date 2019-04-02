194b5d488ccaa0a046dd4907abb7307311ea4552
Giacomo Bagnoli <gbagnoli@gmail.com>
2019-04-02 14:05:23 +0100

    Use two addresses for syncthing for bloodsea and marvin

--- site-cookbooks/bender/attributes/containers.rb
+++ site-cookbooks/bender/attributes/containers.rb
@@ -1,4 +1,9 @@
-# # MARVIN
+def_ipv4 = node['bender']['network']['host']['ipv4']['addrs'][0]
+def_ipv6 = node['bender']['network']['host']['ipv6']['addrs'][1]
+bs_ipv4 = node['bender']['network']['host']['ipv4']['addrs'][1]
+bs_ipv6 = node['bender']['network']['host']['ipv6']['addrs'][1]
+
+# MARVIN
 # id must be unique. Used for ips etc
 default['bender']['containers']['marvin']['id'] = 2
 default['bender']['containers']['marvin']['image'] = 'ubuntu:18.04'
@@ -7,7 +12,8 @@ default['bender']['containers']['marvin']['image'] = 'ubuntu:18.04'
 # [{internal_port: x, external_port: y, ip_version: <ipv4|ipv6|all>, protocol: <tcp|udp|all>}, {}]
 default['bender']['containers']['marvin']['forwarded_ports'] = [
   { protocol: 'udp', external_port: 1195, internal_port: 1194, ip_version: 'all' },
-  { protocol: 'tcp', external_port: 22_000, internal_port: 22_000, ip_version: 'all' }
+  { protocol: 'tcp', external_port: 22_000, internal_port: 22_000, ip_version: 'all',
+    external_ipv4: def_ipv4, external_ipv6: def_ipv6 }
 ]
 # if set, this will nat 1:1 the ipv6 address to the container
 default['bender']['containers']['marvin']['external_ipv6'] = nil
@@ -40,6 +46,9 @@ default['bender']['containers']['pentolino']['volumes'] = nil
 # BLOODSEA
 default['bender']['containers']['bloodsea']['id'] = 5
 default['bender']['containers']['bloodsea']['image'] = 'ubuntu:18.04'
-default['bender']['containers']['bloodsea']['forwarded_ports'] = []
+default['bender']['containers']['bloodsea']['forwarded_ports'] = [
+  { protocol: 'tcp', external_port: 22_000, internal_port: 22_000, ip_version: 'all',
+    external_ipv4: bs_ipv4, external_ipv6: bs_ipv6 }
+]
 default['bender']['containers']['bloodsea']['external_ipv6'] = nil
 default['bender']['containers']['bloodsea']['volumes'] = nil
