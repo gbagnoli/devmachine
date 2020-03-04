04a62da69be61ed87ca2e9abb6db28ade95590a2
Giacomo Bagnoli <gbagnoli@gmail.com>
2020-03-04 23:03:21 +0000

    fix empty attribute

--- site-cookbooks/server/attributes/default.rb
+++ site-cookbooks/server/attributes/default.rb
@@ -4,3 +4,5 @@ default['server']['components']['chef_client_cron']['enabled'] = true
 
 default['server']['chef']['cron']['hour'] = '*'
 default['server']['chef']['cron']['minute'] = '30'
+
+default['chef_client']['config'] = {}
