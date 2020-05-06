d39aace0bce29d9ce4308858a434e1cd4314d79d
Giacomo Bagnoli <gbagnoli@gmail.com>
2020-05-06 10:19:52 +0100

    update ssh key for dario

--- site-cookbooks/server/attributes/users.rb
+++ site-cookbooks/server/attributes/users.rb
@@ -28,8 +28,7 @@ default["server"]["users"]["dario"]["delete"] = false
 default["server"]["users"]["dario"]["id"] = 2002
 default["server"]["users"]["dario"]["shell"] = "/bin/bash"
 default["server"]["users"]["dario"]["ssh_keys"] = [
-  "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQDKWR1plRpIJRL7KoITtE6a+xdRM6Aiq8OWX+rin7XUzoDPygA9zsajwpS7dn6iY1rRkbtdLPiakViHLnpTIwwVLYgUJseWe5MwGHfiDxTs6kr2tK/MMhvue+aBDeclUC1Le/xBlSHjY7+nyScZJrzDVv/EVE3Q4BS879tWSY6QMKHgp+d6ARHP5E4x4J9kaiD+rEBJOweFG6+FuyS2GAj/fmWbolrb+n72EgbDl9txqCRHlQMMKOCsEHj6kztb+tSZlcMOLmUixYMw9Ye1PJ0c7Rt5W6f5QgoDQLgurIiJ7V7ld17TNSzn1ifdsZnEydZaFq91yoHxCs4X+0o0hpxOr2MpSqgd8KQHNm+fLhM8z+lw0lgYS9tOlS3bFskIAOzLxXlSWZa2jFIM26G9R5/mibKBMOcUiogRSI/F8h7DRLACR4IFK0uiRzQdVHfpQ9NBJ0/ZoVXfGx6Paz8p9PkfVP1xoiKb3vgt9CADCAIBwekHdVDYCXzsedUWDkUhtpHzIY8krq8w4x4pbg8EojA9PfgustDVzo5KuDZqLPXL8qYD1IKoPLPsTFIdYbO7UVfCANdx+2AgmP9RrJUz+kBdkXzDqtESmA1IPd66TJwKjzwSRv0MLyjl1KacbpKrQDI7kg3GVSE3KzrrhCy+APcXgjUXpnH6UvyLOX66QomrVQ==",
-  "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKDbt/4A3BDsFZ64eAqiv6JSkSpRsgvsz3zTwwLrkHyz",
+  "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILDS/c+bNRBdYvNvcTSf/ptLWBtOBzf26vc/xQuPhpRC",
 ]
 
 # sonne
