38a42db1c395b5230e7230a01f8fe8ba73b15b66
Giacomo Bagnoli <gbagnoli@gmail.com>
2020-06-06 20:03:54 +0100

    add command to download m3u8 with ffmpeg

--- site-cookbooks/ubik/recipes/packages.rb
+++ site-cookbooks/ubik/recipes/packages.rb
@@ -13,7 +13,7 @@ packages = %w[
   libxslt1-dev libxss1 libxtst6 network-manager-openvpn-gnome openvpn powertop
   python python-apt qemu-kvm rsyslog shellcheck telegram tmux
   ttf-mscorefonts-installer ubuntu-gnome-desktop ufraw unity-tweak-tool
-  xdg-utils wireguard xsel signal-desktop vlc
+  xdg-utils wireguard xsel signal-desktop vlc ffmpeg
 ]
 
 if node["lsb"]["codename"] == "focal"
