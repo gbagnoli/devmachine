c046325c6d7efc73ce41bd676a02f07e6aa15e7c
Giacomo Bagnoli <gbagnoli@gmail.com>
2024-03-30 22:20:16 +0100

    server: support fedora

--- site-cookbooks/server/recipes/default.rb
+++ site-cookbooks/server/recipes/default.rb
@@ -1,13 +1,19 @@
-include_recipe "apt::unattended-upgrades"
+if node.platform_family? "debian"
+  include_recipe "apt::unattended-upgrades"
+  user "ubuntu" do
+    action :remove
+  end
+else
+  # fedora needs crontab
+  package 'cronie'
+end
+
 include_recipe "chef_client_updater" if node["server"]["components"]["chef_client_updater"]["enabled"]
 include_recipe "hardening"
 include_recipe "user" unless node["server"]["components"]["user"]["enabled"] == false
 include_recipe "server::users"
 include_recipe "syncthing" unless node["server"]["components"]["syncthing"]["enabled"] == false
+
 unless node["server"]["components"]["chef_client_cron"]["enabled"] == false
   include_recipe "server::chef_client_cron"
-end
-
-user "ubuntu" do
-  action :remove
 end
