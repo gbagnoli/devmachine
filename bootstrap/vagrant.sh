e859017ff0e79dee1eabedff3090f3ae94d84157
Giacomo Bagnoli <gbagnoli@gmail.com>
2024-03-31 15:23:24 +0200

    Run vagrant bootstrap as vagrant user

--- bootstrap/vagrant.sh
+++ bootstrap/vagrant.sh
@@ -15,7 +15,8 @@ getent passwd giacomo &>/dev/null || (
   useradd -u 2000 -g giacomo -G "$group" -m -s /bin/bash giacomo
   echo "giacomo        ALL=(ALL)       NOPASSWD: ALL" > /etc/sudoers.d/giacomo
 )
-sudo -i which pipenv || sudo -i pip3 install --user pipenv
-pipenv="$(sudo -i python3 -m site --user-base)"/bin/pipenv
-sudo -i bash -c "cd /vagrant && $pipenv install"
-sudo -i bash -c "cd /vagrant && CHEF_LICENSE=accept $pipenv run -- fab run -H $role"
+sudo="sudo -i -u vagrant"
+$sudo which pipenv || $sudo pip3 install --break-system-packages pipenv
+pipenv="$($sudo python3 -m site --user-base)"/bin/pipenv
+$sudo bash -c "cd /vagrant && $pipenv install"
+$sudo bash -c "cd /vagrant && CHEF_LICENSE=accept $pipenv run -- fab run -H $role"
