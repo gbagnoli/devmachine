6a7e60a900a04b7acf72922e546fbc76d69529a7
Giacomo Bagnoli <gbagnoli@gmail.com>
2024-04-11 22:38:01 +0200

    calculon: install podman and crun from repo

--- site-cookbooks/calculon/attributes/default.rb
+++ site-cookbooks/calculon/attributes/default.rb
@@ -4,11 +4,21 @@ default["calculon"]["rocky"]["btfrs_progs"] = {
   packages: %w{btrfs-progs btrfs-progs-devel libbtrfs libbtrfsutil python3-btrfsutil}
 }
 
-default["calculon"]["rocky"]["podman"] = {
-  version: "5.0.0-1.el9",
-  url: "https://rpmfind.net/linux/centos-stream/9-stream/AppStream/x86_64/os/Packages/",
-  package: "podman",
+default["calculon"]["rocky"]["podman"]["git"] = {
+  url: "https://github.com/containers/podman.git",
+  tag: "v5.0.1",
+  # btrfs-devel is already installed from downloaded rpm in rocky
+  deps:  %w{go systemd-devel gpgme-devel libseccomp-devel},
 }
+
+# rubocop:disable Layout/LineLength
+default["calculon"]["rocky"]["crun"]["git"] = {
+  url: "https://github.com/containers/crun.git",
+  tag: "1.14.4",
+  deps: %w{make automake autoconf gettext libtool gcc libcap-devel systemd-devel yajl-devel glibc-static libseccomp-devel},
+}
+# rubocop:enable Layout/LineLength
+
 
 default["calculon"]["data"]["username"] = "media"
 default["calculon"]["data"]["group"] = "data"
--- site-cookbooks/calculon/recipes/containers.rb
+++ site-cookbooks/calculon/recipes/containers.rb
@@ -1,25 +1,72 @@
 if platform?("rocky")
   # in a spiral of sadness, rocky's podman
   # doesn't have btrfs enabled
-  conf = node["calculon"]["rocky"]["podman"]
-  arch = node["kernel"]["machine"]
-  rpm = "#{conf[:package]}-#{conf[:version]}.#{arch}.rpm"
-  remote = "#{conf[:url]}/#{rpm}"
-  local = "#{Chef::Config[:file_cache_path]}/#{rpm}"
-
-  remote_file local do
-    source remote
-    action :create
+  # so install the version in the repo, lock it down
+  # then compile podman (!!!) and substitute the files
+  package "podman" do
+    package_name %w{podman crun python3-dnf-plugin-versionlock.noarch}
   end
 
-  execute "install podman" do
-    command "dnf install --assumeyes #{local}"
-    not_if "dnf list installed podman | grep -q #{conf[:version]}"
-  end
-end
+  podman = node["calculon"]["rocky"]["podman"]["git"]
+  crun = node["calculon"]["rocky"]["crun"]["git"]
 
-package "virt" do
-  package_name %w(crun podman)
+  package "podman deps" do
+    package_name podman[:deps]
+  end
+
+  package "crun deps" do
+    package_name crun[:deps]
+  end
+
+  execute "lock podman" do
+    command "dnf versionlock podman"
+    not_if "dnf versionlock | grep -q podman"
+  end
+
+  execute "lock crun" do
+    command "dnf versionlock crun"
+    not_if "dnf versionlock crun | grep -q crun"
+  end
+
+  git "#{Chef::Config[:file_cache_path]}/podman" do
+    repository podman[:url]
+    action :sync
+    user "root"
+    revision podman[:tag]
+    notifies :run, "bash[build and install podman]", :immediately
+  end
+
+  bash "build and install podman" do
+    action :nothing
+    cwd "#{Chef::Config[:file_cache_path]}/podman"
+    code <<-EOH
+      make
+      cp -v bin/* /usr/bin
+    EOH
+  end
+
+  git "#{Chef::Config[:file_cache_path]}/crun" do
+    repository crun[:url]
+    action :sync
+    user "root"
+    revision crun[:tag]
+    notifies :run, "bash[build and install crun]", :immediately
+  end
+
+  bash "build and install crun" do
+    action :nothing
+    cwd "#{Chef::Config[:file_cache_path]}/crun"
+    code <<-EOH
+      ./autogen.sh
+      ./configure --prefix=/usr
+      make -j8
+      make install
+    EOH
+  end
+else
+  package "virt" do
+    package_name %w(crun podman)
+  end
 end
 
 execute "reset podman" do
