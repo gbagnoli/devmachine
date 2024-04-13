074a0f76d2cf28969101bef31ab033bdd4256d19
Giacomo Bagnoli <gbagnoli@gmail.com>
2024-04-13 18:06:01 +0200

    Better literals for the quadlet units. Expose syncthing over ipv6

--- site-cookbooks/calculon/recipes/containers.rb
+++ site-cookbooks/calculon/recipes/containers.rb
@@ -18,14 +18,14 @@ end
 
 podman_network "calculon" do
   config(
-    Network: [
-      "Driver=bridge",
-      "IPv6=True",
-      "Subnet=#{node["calculon"]["network"]["containers"]["ipv4"]["network"]}",
-      "Subnet=#{node["calculon"]["network"]["containers"]["ipv6"]["network"]}",
-      "Gateway=#{node["calculon"]["network"]["containers"]["ipv4"]["addr"]}",
-      "Gateway=#{node["calculon"]["network"]["containers"]["ipv6"]["addr"]}",
-    ]
+    Network: %W{
+      Driver=bridge
+      IPv6=True
+      Subnet=#{node["calculon"]["network"]["containers"]["ipv4"]["network"]}
+      Subnet=#{node["calculon"]["network"]["containers"]["ipv6"]["network"]}
+      Gateway=#{node["calculon"]["network"]["containers"]["ipv4"]["addr"]}
+      Gateway=#{node["calculon"]["network"]["containers"]["ipv6"]["addr"]}
+    }
   )
 end
 
@@ -37,25 +37,29 @@ end
 
 podman_container "syncthing" do
   config(
-    Container: [
-      "Image=syncthing.image",
-      "Environment=PUID=#{node["calculon"]["data"]["uid"]}",
-      "Environment=PGID=#{node["calculon"]["data"]["gid"]}",
-      "PublishPort=8384:8384",
-      "PublishPort=22000:22000/tcp",
-      "PublishPort=22000:22000/udp",
-      "Volume=#{node["calculon"]["storage"]["paths"]["sync"]}:/var/syncthing",
-      "HostName=sync.tigc.eu",
-      "Network=calculon.network",
-    ],
-    Service: [
-      "Restart=Always"
-    ],
+    Container: %W{
+      Image=syncthing.image
+      Environment=PUID=#{node["calculon"]["data"]["uid"]}
+      Environment=PGID=#{node["calculon"]["data"]["gid"]}
+      PublishPort=[::1]:8384:8384
+      PublishPort=[::]:22000:22000/tcp
+      PublishPort=[::]:22000:22000/udp
+      PublishPort=22000:22000/tcp
+      PublishPort=22000:22000/udp
+      Volume=#{node["calculon"]["storage"]["paths"]["sync"]}:/var/syncthing
+      HostName=sync.tigc.eu
+      Network=calculon.network
+    },
+    Service: %w{
+      Restart=always
+    },
     Unit: [
       "Description=Start Syncthing file synchronization",
+      "After=network-online.target",
+      "Wants=network-online.target",
     ],
-    Install: [
-      "WantedBy=multi-user.target"
-    ]
+    Install: %w{
+      WantedBy=multi-user.targe
+    }
   )
 end
