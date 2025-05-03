c4787d24beabc1d8ee64816e2db3240dab7d34f8
Giacomo Bagnoli <gbagnoli@gmail.com>
2025-05-03 15:54:20 +0200

    Only advertise lan from boxy

--- site-cookbooks/rupik/recipes/tailscale.rb
+++ site-cookbooks/rupik/recipes/tailscale.rb
@@ -4,8 +4,7 @@ node.override["tailscale"]["podman"]["group"] = "nogroup"
 node.override["tailscale"]["podman"]["config_dir"] = "#{node["rupik"]["storage"]["path"]}/tailscale"
 node.override["tailscale"]["podman"]["export_resolv.conf"] = true
 node.override["tailscale"]["podman"]["extra_env"] = {
-  "TS_ROUTES" => node["rupik"]["lan"]["ipv4"]["network"],
-  "TS_EXTRA_ARGS" => '"--advertise-exit-node --accept-routes --snat-subnet-routes=false"',
+  "TS_EXTRA_ARGS" => '--advertise-exit-node',
   "TS_ACCEPT_DNS" => "false",
 }
 include_recipe "tailscale::install"
