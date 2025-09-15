33cee1735332d7e1e63729ccc440fe6b312746e3
Giacomo Bagnoli <gbagnoli@gmail.com>
2025-09-15 22:35:57 +0200

    Remove old distro support from user::photos

--- site-cookbooks/user/recipes/photos.rb
+++ site-cookbooks/user/recipes/photos.rb
@@ -1,18 +1,7 @@
 # frozen_string_literal: true
 
-if platform?("ubuntu") && ! %w(focal jammy noble).include?(node["lsb"]["codename"])
-  %w(exiftool python-wxgtk3.0 python-pil python-unidecode
-   libfreeimage3 libfontconfig1:i386 libxt6:i386 libxrender1:i386
-   libxext6:i386 libgl1-mesa-glx:i386 libgl1-mesa-dri:i386 libcurl3:i386
-   libgssapi-krb5-2:i386 librtmp1:i386 libsm6:i386 libice6:i386
-   libuuid1:i386 fonts-liberation lsb-core libglu1-mesa
-   gpsbabel gpsbabel-gui libqtcore4 python-tz rename).each do |pkg|
-    package pkg
-  end
-else
-  package "photo_deps" do
-    package_name %w(exiftool gpsbabel python3-tz rename)
-  end
+package "photo_deps" do
+  package_name %w(exiftool gpsbabel python3-tz rename lsb-release)
 end
 
 user = node["user"]["login"]
@@ -29,17 +18,6 @@ file "#{home}/.local/bin/gpicsync" do
     #!/bin/bash
     cd #{home}/.local/src/gpicsync/src/
     /usr/bin/python3 gpicsync.py "$@"
-          EOC
-  owner user
-  group node["user"]["group"]
-  mode '750'
-end
-
-file "#{home}/.local/bin/gpicsync-GUI" do
-  content <<~EOC
-    #!/bin/bash
-    cd #{home}/.local/src/gpicsync/src/
-    /usr/bin/python gpicsync-GUI.py "$@"
           EOC
   owner user
   group node["user"]["group"]
@@ -64,14 +42,3 @@ if node["user"]["install_photo_process"]
     action :sync
   end
 end
-
-version = node["lsb"]["release"]
-
-apt_repository "gpxsee" do
-  uri "http://download.opensuse.org/repositories/home:/tumic:/GPXSee/xUbuntu_#{version}/"
-  distribution "/"
-  components [""]
-  key "https://download.opensuse.org/repositories/home:tumic:GPXSee/xUbuntu_#{version}/Release.key"
-end
-
-package "gpxsee"
