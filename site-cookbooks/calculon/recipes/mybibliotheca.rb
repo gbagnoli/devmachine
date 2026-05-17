6caf043ed5367887b4ec5cc533d27df241290737
Giacomo Bagnoli <gbagnoli@gmail.com>
2026-05-17 13:59:30 +0200

    Uninstall mybibliotheca

--- site-cookbooks/calculon/recipes/mybibliotheca.rb
+++ site-cookbooks/calculon/recipes/mybibliotheca.rb
@@ -3,61 +3,24 @@ web_port = 5054
 
 user = node["calculon"]["data"]["username"]
 group = node["calculon"]["data"]["group"]
-secrets = node["calculon"]["mybibliotheca"]["secrets"]
 domain = node["calculon"]["mybibliotheca"]["domain"]
 backup_path = "#{node["calculon"]["storage"]["paths"]["backups"]}/mybibliotheca"
 mybibliotheca_root = node["calculon"]["storage"]["paths"]["mybibliotheca"]
 data = "#{mybibliotheca_root}/data"
-
-if secrets.empty?
-  Chef::Log.info('calculon: missing secrets for wanderer')
-  raise
-end
-if domain.nil? || domain.empty?
-  Chef::Log.info('calculon: missing domain for wanderer')
-  raise
-end
-public_url = "https://#{domain}"
 envfile="/etc/containers/systemd/mybibliotheca.env"
-
-%w{secret_key security_password_salt}.each do |s|
-  if secrets[s].nil? || secrets[s].empty?
-    Chef::Log.info("calculon: missing secret calculon.wanderer.#{s} for wanderer")
-    raise
-  end
-end
-
-calculon_btrfs_volume mybibliotheca_root do
-  owner user
-  group group
-  mode "2775"
-  setfacl true
-end
-
-[
-  "data",
-].each do |dir|
-  directory "#{mybibliotheca_root}/#{dir}" do
-      owner user
-      group group
-      mode "2755"
-  end
-end
-
-config = {
-  "SECRET_KEY" => secrets["secret_key"],
-  "SECURITY_PASSWORD_SALT" => secrets["security_password_salt"],
-  "TIMEZONE" => "Europe/Madrid",
-  "WORKERS" => "12"
-}
-
-file envfile do
-  mode "0400"
-  content config.sort.map {|k,v| "#{k.upcase}=#{v}"}.join("\n")
-end
 
 container = "mybibliotheca"
 service = "#{container}.service"
+
+service service do
+  action :stop
+end
+
+
+file envfile do
+  mode "0400"
+  action :delete
+end
 
 podman_container container do
   config(
@@ -78,16 +41,26 @@ podman_container container do
       "WantedBy=multi-user.target default.target"
     ]
   )
+  action :delete
 end
 
-service service do
-  action :start
-  subscribes :restart, "file[#{envfile}]"
+calculon_btrfs_volume mybibliotheca_root do
+  owner user
+  group group
+  mode "2775"
+  setfacl true
+  action :delete
 end
 
-calculon_www_link "Books" do
-  category "Apps"
-  url public_url
+[
+  "data",
+].each do |dir|
+  directory "#{mybibliotheca_root}/#{dir}" do
+      owner user
+      group group
+      mode "2755"
+      action :delete
+  end
 end
 
 addr4 = node["calculon"]["network"]["containers"]["ipv4"]["addr"]
@@ -102,6 +75,7 @@ podman_nginx_vhost domain do
     emails: node["calculon"]["www"]["user_emails"],
     port: 4300
   )
+  action :delete
 end
 
 systemd_unit "mybibliotheca-backup-files.service" do
@@ -116,7 +90,7 @@ ExecStart=/usr/bin/rsync -aAXHv --mkpath --delete #{mybibliotheca_root}/ #{backu
 [Install]
 WantedBy=default.target
   EOH
-  action %i(create enable)
+  action %i(disable delete)
 end
 
 systemd_unit "wanderer-backup-files.timer" do
@@ -132,5 +106,5 @@ Persistent=true
 [Install]
 WantedBy=timers.target
   EOH
-  action %i(create enable start)
+  action %i(stop disable delete)
 end
